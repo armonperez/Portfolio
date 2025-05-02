548798585d053342fc40557a8b0ac9786d88cd08
Armon Perez <armonperez16@gmail.com>
2025-05-01 23:37:58 -0700

    Adding images and resume. Fixing alignment issues and Animations

--- js/script.js
+++ js/script.js
@@ -1,12 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const cards = document.querySelectorAll(".project-card");
+    const faders = document.querySelectorAll('.fade-in');
   
-    cards.forEach((card, index) => {
-      setTimeout(() => {
-        card.classList.add("visible");
-      }, index * 1000); // 1 second delay between each
+    const appearOptions = {
+      threshold: .3
+    };
+  
+    const appearOnScroll = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('visible');
+          observer.unobserve(entry.target); // Optional: stops observing once visible
+        }
+      });
+    }, appearOptions);
+  
+    faders.forEach(fader => {
+      appearOnScroll.observe(fader);
     });
   });
-  
   
   
